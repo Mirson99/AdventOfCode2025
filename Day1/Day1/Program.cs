775260a730698953afac65cfd307f1272b7c0296
Mirson99 <lukmir99@gmail.com>
2025-12-02 13:12:55 +0100

    Add first and second day

---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,84 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+
+// Part One
+// List<string> instructions = File.ReadAllLines("../../../../input.txt").ToList();
+//
+// var currentValue = 50;
+// var resultCount = 0;
+//
+// foreach (var instruction in instructions)
+// {
+//     var direction = instruction[0];
+//     var value = int.Parse(instruction.Substring(1));
+//     value = value % 100;
+//     if (direction == 'R')
+//     {
+//         currentValue += value;
+//         if (currentValue > 99)
+//         {
+//             currentValue -=100;
+//         }
+//     }
+//
+//     if (direction == 'L')
+//     {
+//         currentValue -= value;
+//         if(currentValue < 0) 
+//         {
+//             currentValue += 100;
+//         }
+//     }
+//     
+//     if (currentValue == 0)
+//         resultCount++;
+// }
+//
+// Console.WriteLine(resultCount);
+
+
+// Part Two
+
+List<string> instructions = File.ReadAllLines("../../../../input.txt").ToList();
+
+var currentValue = 50;
+var resultCount = 0;
+
+foreach (var instruction in instructions)
+{
+    var direction = instruction[0];
+    var value = int.Parse(instruction.Substring(1));
+    resultCount += (value / 100);
+    value = value % 100;
+    if (direction == 'R')
+    {
+        currentValue += value;
+        if (currentValue > 99)
+        {
+            resultCount++;
+            currentValue -= 100;
+        }
+    }
+
+    if (direction == 'L')
+    {
+        var initialValue = currentValue;
+        currentValue -= value;
+        if(currentValue < 0)
+        {
+            currentValue += 100;
+
+            if (initialValue != 0)
+                resultCount++;
+        }
+        else if (currentValue == 0)
+            resultCount++;
+    }
+}
+
+Console.WriteLine(resultCount);
+
+
+
---
+++ Day2/Day2/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Hello, World!");
+
+string pattern = @"^([1-9]\d*)\1$";
+string pattern2 = @"^([1-9]\d*)\1+$";
+long result = 0;
+
+string content = File.ReadAllText("../../../../input2.txt");
+
+List<IdRange> idRanges = content
+    .Split(',')
+    .Select(item => item.Trim())
+    .Where(item => !string.IsNullOrEmpty(item))
+    .Select(item =>
+    {
+        var parts = item.Split('-');
+        return new IdRange
+        {
+            Start = long.Parse(parts[0]),
+            End = long.Parse(parts[1])
+        };
+    })
+    .ToList();
+
+foreach (var range in idRanges)
+{
+    for (long i = range.Start; i < range.End; i++)
+    {
+        bool match = Regex.IsMatch(i.ToString(), pattern2);
+        if (match)
+            result += i;
+    }
+}
+
+Console.WriteLine(result);
+
+
+
+public class IdRange
+{
+    public long Start { get; set; }
+    public long End { get; set; }
+}
+
